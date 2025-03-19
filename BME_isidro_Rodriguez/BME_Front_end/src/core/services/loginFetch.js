703ce370fb5e1215ca2d71d8ea5da6fc1a90ee9b
isidro9494 <renplagnes@gmail.com>
2025-03-19 21:14:53 +0100

    finishLoginfront

--- BME_isidro_Rodriguez/BME_Front_end/src/core/services/loginFetch.js
+++ BME_isidro_Rodriguez/BME_Front_end/src/core/services/loginFetch.js
@@ -2,22 +2,34 @@ export const doLoginFetch = async (username, password) => {
     const res = await fetch('http://localhost:3000/auth/login', {
       method: 'POST',
       headers: {
-        'content-type': 'application/json', 
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ username, password }),
     });
   
+    if (!res.ok) {
+      const errorData = await res.json(); 
+      throw new Error(errorData.message || 'Error en el login');
+    }
   
+    const data = await res.json();
+    return data;
   };
 
-export const createUser = async (newUser) => {
+  export const createUser = async (newUser) => {
     const res = await fetch('http://localhost:3000/auth/register', {
         method: 'POST',
         headers: {
-            'content-type': 'Application/json'
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            newUser
-        })
-    })
-}
+        body: JSON.stringify(newUser), 
+    });
+
+    if (!res.ok) {
+        const errorData = await res.json(); 
+        throw new Error(errorData.message || 'Error en el registro');
+    }
+
+    const data = await res.json(); 
+    return data; 
+};
